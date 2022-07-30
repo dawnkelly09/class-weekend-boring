7f082b9933eef73c15834c3c6d7d991e8b4d3ace
dawnkelly09 <dawnkelly09@gmail.com>
2022-07-29 17:11:47 -0700

    sync changes

--- scripts/main.js
+++ scripts/main.js
@@ -6,11 +6,11 @@ function check() {
   
 
   //Conditionals go here
-  if (day=='tuesday'||day=='thursday') {
+  if (day==='tuesday'||day==='thursday') {
     let message = "It is a class day!"
     document.querySelector('#placeToSee').innerText = message
   }
-  else if (day=='saturday'||day=='sunday') {
+  else if (day==='saturday'||day==='sunday') {
     message = "It is the weekend!"
     document.querySelector('#placeToSee').innerText = message
   }
